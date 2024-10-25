b0c6920ab59d965723ae04e9fcf8299e01d93ca3
tushar <tushar158gupta@gmail.com>
2024-10-26 03:25:43 +0530

    1

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const cors = require('cors');
-const express = require('express');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db.js";
+import User from "./User.js";
+
+dotenv.config();
 const app = express();
 
 // Enable CORS for the React frontend
-app.use(cors({
-  origin: 'http://localhost:5173', // React frontend URL
-  credentials: true, // Allow cookies and authorization headers
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // React frontend URL
+    credentials: true, // Allow cookies and authorization headers
+  })
+);
 
 // Other middleware and routes
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  // Your login logic
-  res.json({ message: ' tushar' });
-});
-app.get('/api/v1/auth/login', (req, res) => {
-  // Your login logic
-  res.json({ message: 'Login successful' });
-});
-app.post('/api/v1/auth/login', (req, res) => {
-  // Your login logic
-  res.json({ message: 'Login successful' });
+app.get("/", async (req, res) => {
+  const users = await User.find({});
+
+  res.json({ users });
 });
 
 app.listen(3000, () => {
-  console.log('Backend running on http://localhost:3000');
+  connectDB();
+  console.log("Backend running on http://localhost:3000");
 });
