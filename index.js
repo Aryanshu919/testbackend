0e99f9b693f211f309fc84cac8e517f3a1316e32
Tushar Gupta <tushar158gupta@gmail.com>
2024-11-16 22:16:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // Enable CORS for the React frontend
 app.use(
   cors({
-    origin: "http://localhost:5173", // React frontend URL
+    origin: "*", // React frontend URL
     credentials: true, // Allow cookies and authorization headers
   })
 );
